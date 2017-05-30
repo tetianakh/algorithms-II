b1669af3660488060c10a94637e0b7d4b457a0de
Tetiana Khotiaintseva <tetiana.khot@gmail.com>
2017-05-31 00:00:38 +0200

    Graph class

---
+++ graphs/graph.py
@@ -0,0 +1,20 @@
+
+
+class Graph:
+    def __init__(self, V):
+        self._V = V
+        self._adj = [set() for _ in range(V)]
+
+    def add_edge(self, v, w):
+        self._adj[v].add(w)
+        self._adj[w].add(v)
+
+    def adjacent(self, v):
+        return iter(self._adj[v])
+
+
+def test_added_edge_is_adjacent():
+    graph = Graph(2)
+    graph.add_edge(0, 1)
+    assert list(graph.adjacent(0)) == [1]
+    assert list(graph.adjacent(1)) == [0]
